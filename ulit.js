9b47b99aa8ad8d4121ee6a747bdbe88601b42c86
Seth-the-cat <tyicetea13@gmail.com>
2023-08-21 20:32:31 -0500

    clean up

--- ulit.js
+++ ulit.js
@@ -1,12 +1,3 @@
-function showorno() {
-  var x = document.getElementById("myDiv");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
 function darkMode() {
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark')
@@ -23,23 +14,19 @@ function darkMode() {
         // Whenever the user explicitly chooses to respect the OS preference
         localStorage.removeItem('theme')
 }
+function lightMode() {
+    if (localStorage.theme === 'light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches)) {
+            document.documentElement.classList.add('light')
+        } else {
+            document.documentElement.classList.remove('light')
+        }
 
-// function mapsapple(){
-// let cords = document.createElement("a");np
-//   if(navigator.userAgent.match(/Mac|iPhone|iPad/)){
-//     jizodb.1jizo.image
-//   }
+        // Whenever the user explicitly chooses light mode
+        localStorage.theme = 'light'
 
-// }
+        // Whenever the user explicitly chooses dark mode
+        localStorage.theme = 'dark'
 
-// import exifr from './node_modules/exifr/dist/lite.esm.js';
-
-
-// import exifr from 'exifr'
-
-const uploadedImage = document.getElementById('image');
-
-uploadedImage.onchange = function () {
-  console.log(uploadedImage.files[0]);
-  exifr.gps(uploadedImage.files[0]).then(data => console.log(data))
+        // Whenever the user explicitly chooses to respect the OS preference
+        localStorage.removeItem('theme')
 }
